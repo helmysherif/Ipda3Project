d11dd61b395dd405718cd2fb0e29a495175db604
Sherif Helmy <helmysherif792@gmail.com>
2020-04-04 13:59:54 +0200

    ipda3 project

---
+++ assets/js/homepage.js
@@ -0,0 +1,34 @@
+$('.carousel2').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
